da2b901a1224ce8c831e5778323e9058c8fe7077
Renz Jay Bayeng <144974361+BayRJ@users.noreply.github.com>
2025-02-11 07:52:08 +0000

    first commit

---
+++ blade-demo2/resources/views/terms.blade.php
@@ -0,0 +1,11 @@
+@extends('layout.layout')
+
+@section('content')
+    <h1>
+        Terms
+    </h1>
+    <div>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt ratione, voluptas provident sapiente, labore repellat tenetur esse enim, temporibus tempora eius animi officia a laboriosam veritatis voluptate error. Cum cupiditate, error pariatur a fugiat necessitatibus dolore voluptates sapiente, atque neque perferendis? Pariatur delectus tenetur perspiciatis eveniet tempore facere officiis repellat dolor odit? Necessitatibus tenetur ad in optio natus, asperiores sequi.
+    </div>
+@endsection
+
